3fd433b264c166aea3a5798ab92905e863d9c106
CellBuuZer <leonardosolis752@gmail.com>
2024-05-02 16:24:01 -0500

    Added ÃÃsupport component, register component, profile component and booking component with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import {LoginViewComponent} from "./components/login-view/login-view.component";
 import {RegisterViewComponent} from "./components/register-view/register-view.component";
 import {ServicesContentComponent} from "./components/services-content/services-content.component";
 import {VeterinaryContentComponent} from "./components/veterinary-content/veterinary-content.component";
+import {SupportContentComponent} from "./components/support-content/support-content.component";
+import {BookingContentComponent} from "./components/booking-content/booking-content.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeContentComponent },
+  { path: 'booking', component: BookingContentComponent },
   { path: 'login', component: LoginViewComponent },
   { path: 'signup', component: RegisterViewComponent },
   { path: 'services', component: ServicesContentComponent },
   { path: 'veterinary', component: VeterinaryContentComponent },
+  { path: 'contact-us', component: SupportContentComponent },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: '**', redirectTo: 'login' },
 ];
---
+++ src/app/components/booking-content/booking-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-content',
+  templateUrl: './booking-content.component.html',
+  styleUrl: './booking-content.component.css'
+})
+export class BookingContentComponent {
+  longText= ' ';
+}
