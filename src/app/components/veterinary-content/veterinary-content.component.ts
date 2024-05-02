deda0940a7a02f837bb5642811d9506ef7f4ab1b
Andrea <armix182002@gmail.com>
2024-05-02 12:41:32 -0500

    Add veterinary component

---
+++ src/app/components/veterinary-content/veterinary-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-veterinary-content',
+  templateUrl: './veterinary-content.component.html',
+  styleUrl: './veterinary-content.component.css'
+})
+export class VeterinaryContentComponent {
+
+}
