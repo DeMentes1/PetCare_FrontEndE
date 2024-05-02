714927607898e6529afe62b159547634f99c68f6
CellBuuZer <leonardosolis752@gmail.com>
2024-05-01 21:59:17 -0500

    Added Register Resources with Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { RegisterViewComponent } from './components/register/register-view/register-view.component';
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {MatLabel} from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormField,
+    MatInput,
+    MatIcon,
+    MatIconButton,
+    MatCheckbox,
+    MatSlideToggle,
+    MatButton,
+    MatLabel
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/components/register/register-view/register-view.component.ts
+++ src/app/components/register/register-view/register-view.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './register-view.component.css'
 })
 export class RegisterViewComponent {
-
+  hide = true;
 }
